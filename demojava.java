a6d43bf7542589da4da5e700b52fe04e49389d02
Gaffar07 <39920290+Gaffar07@users.noreply.github.com>
2020-12-23 12:35:29 +0530

    Update demojava.java
    final commit

--- demojava.java
+++ demojava.java
@@ -6,8 +6,8 @@ class demojava
 
 	public static void main(String args[])
 	{
-		int a=100
-		System.out.println("Hi jenkins!!!"+a);
+		
+		System.out.println("Hi jenkins!!!");
 		System.out.println("Hi jenkins!!!");
 		System.out.println("Hi jenkins!!!");
 		
